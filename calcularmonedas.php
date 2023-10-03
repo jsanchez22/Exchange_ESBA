d5789b1b19b7ee5b29b123b6205bda47072908d8
Jeanfranco Sanchez (CTGB) <jeanfranco.sanchez@AES.COM>
2023-10-03 09:25:02 -0300

    finish

--- calcularmonedas.php
+++ calcularmonedas.php
@@ -7,7 +7,7 @@ if (isset($_POST["numero"])) {
     $euro_oficial = number_format($numero / $data['oficial_euro']['value_buy'],2);
     $euro_blue = number_format($numero / $data['blue_euro']['value_buy'],2);
     echo "<h1>Pesos : $ $numero</h1>";
-    echo "<div class='mb-6 bg-blue-800 p-6  text-2xl text-white flex flex-col justify-between w-96 h-96  p-1.5 rounded-2xl'>";
+    echo "<div class='mb-6 bg-blue-800 p-6  text-2xl text-white flex flex-col justify-around w-96 h-96  p-1.5 rounded-2xl'>";
     
     echo "<div class='flex  justify-between'><h1>Dolar Oficial:</h1><h1> $ $dolar_oficial</h1></div>";
     echo "<div class='flex justify-between'><h1>Dolar Blue:</h1><h1> $ $dolar_blue</h1></div>";
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php include('./layout/header.php'); ?>
-<H1 class="text-2xl bg-red-800 text-white p-1 text-center">COTIZACIONES DE MONEDAS EXTRANJERAS</H1>
-<div class= "container flex h-4/6 justify-around place-items-center ">
+<H1 class="text-2xl bg-red-800 text-white p-1 text-center ">COTIZACIONES DE MONEDAS EXTRANJERAS</H1>
+<div class= "container flex  grid grid-cols-1 md:grid-cols-4  h-4/6 justify-around place-items-center md:flex-wrap">
 
     <?php
     include('./api_request.php');
@@ -23,7 +23,7 @@
             new Moneda('blue_euro', 'Euro Blue')
         );
         foreach ($indice_monedas as $moneda) {
-            echo '<div  class=" flex flex-col w-64 h-64  p-3 rounded-2xl text-white  bg-blue-800 transition-transform transform hover:scale-110 " >';
+            echo '<div  class=" flex flex-col w-64 h-64  p-3 rounded-2xl text-white  bg-blue-800 md:my-1 transition-transform transform hover:scale-110 " >';
             echo '<h1 class=" text-center text-2xl mt-3">'. $moneda->name .'</h1>';
             echo '<h2 class=" text-center  text-xl mt-3">Valor promedio: $ ' . $data[$moneda->moneda]['value_avg'] . '</h2>';
             echo '<h2 class=" text-center text-xl mt-3 bg-green-500">Valor de venta: $' . $data[$moneda->moneda]['value_sell'] . '</h2>';
